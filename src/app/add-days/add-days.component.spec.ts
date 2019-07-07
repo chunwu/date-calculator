cf669e7cf78019a006f98af1ca4f701827c75e24
Chun Wu <chunwu@claimvantage.com>
2019-07-07 15:15:48 +1000

    More test cases for the addDays function

--- src/app/add-days/add-days.component.spec.ts
+++ src/app/add-days/add-days.component.spec.ts
@@ -1,6 +1,6 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
-import { AddDaysComponent, ActionType } from './add-days.component';
+import { AddDaysComponent, ActionType, DayType } from './add-days.component';
 
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE } from '@angular/material/core';
@@ -23,7 +23,6 @@ describe('AddDaysComponent', () => {
       imports: [ BrowserAnimationsModule, ReactiveFormsModule, MatInputModule, MatDatepickerModule, MatFormFieldModule, MatRadioModule, MatSelectModule ],
       declarations: [ AddDaysComponent ],
       providers: [
-        {provide: MAT_DATE_LOCALE, useValue: 'en-au'},
         {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
         {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS}
       ],
@@ -53,11 +52,34 @@ describe('AddDaysComponent', () => {
     expect(component.result).toBeFalsy();
   });
 
-  it("should add calendar days", function() {
+  it("should add/subtract calendar days", function() {
     doTestAddOrSubtractCalendarDays(newDate('2019-06-01'), ActionType.Add, 0, 0, 3, 'Tue Jun 04 2019');
     doTestAddOrSubtractCalendarDays(newDate('2019-06-01'), ActionType.Add, 0, 2, 0, 'Sat Jun 15 2019');
     doTestAddOrSubtractCalendarDays(newDate('2019-06-01'), ActionType.Add, 1, 0, 0, 'Mon Jun 01 2020');
     doTestAddOrSubtractCalendarDays(newDate('2019-06-01'), ActionType.Add, 1, 2, 3, 'Thu Jun 18 2020');
+
+    doTestAddOrSubtractCalendarDays(newDate('2019-06-01'), ActionType.Subtract, 0, 0, 3, 'Wed May 29 2019');
+    doTestAddOrSubtractCalendarDays(newDate('2019-06-01'), ActionType.Subtract, 0, 2, 0, 'Sat May 18 2019');
+    doTestAddOrSubtractCalendarDays(newDate('2019-06-01'), ActionType.Subtract, 1, 0, 0, 'Fri Jun 01 2018');
+    doTestAddOrSubtractCalendarDays(newDate('2019-06-01'), ActionType.Subtract, 1, 2, 3, 'Tue May 15 2018');
+  });
+
+  it("should add/subtract business days", function() {
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Add, 1, 'Mon Jun 03 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Add, 2, 'Tue Jun 04 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Add, 3, 'Wed Jun 05 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Add, 4, 'Thu Jun 06 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Add, 5, 'Fri Jun 07 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Add, 6, 'Mon Jun 10 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Add, 7, 'Tue Jun 11 2019');
+
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Subtract, 1, 'Fri May 31 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Subtract, 2, 'Thu May 30 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Subtract, 3, 'Wed May 29 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Subtract, 4, 'Tue May 28 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Subtract, 5, 'Mon May 27 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Subtract, 6, 'Fri May 24 2019');
+    doTestAddOrSubtractBusinessDays(newDate('2019-06-01'), ActionType.Subtract, 7, 'Thu May 23 2019');
   });
 
   function newDate(dateStr: string) {
@@ -71,6 +93,7 @@ describe('AddDaysComponent', () => {
       weeks: number, 
       days: number,
       expectedResult: string) {
+    component.addDaysForm.controls.dayType.value = DayType.Calendar;
     component.addDaysForm.controls.start.value = startDate;
     component.addDaysForm.controls.action.value = action;
     component.addDaysForm.controls.years.value = years;
@@ -80,5 +103,17 @@ describe('AddDaysComponent', () => {
     expect(component.result).toEqual(expectedResult);
   };
 
+  function doTestAddOrSubtractBusinessDays(
+      startDate, 
+      action: ActionType, 
+      days: number, 
+      expectedResult) {
+    component.addDaysForm.controls.dayType.value = DayType.Business;
+    component.addDaysForm.controls.start.value = startDate;
+    component.addDaysForm.controls.action.value = action;
+    component.addDaysForm.controls.days.value = days;
+    component.calculate()
+    expect(component.result).toEqual(expectedResult);
+  };
 });
 
