412caaeec0cfe1e3140950082a2252af5c536891
Chun Wu <mail.wuchun@gmail.com>
2019-07-20 10:37:47 +1000

    Fix tests

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,52 +1,21 @@
 import { TestBed, async } from '@angular/core/testing';
 
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
-import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { MatCheckboxModule, MatInputModule, MatTabsModule } from '@angular/material';
-import { MatDatepickerModule } from '@angular/material/datepicker';
-import { MatFormFieldModule } from '@angular/material/form-field';
-import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
-import { MatRadioModule } from '@angular/material/radio';
-import { MatSelectModule } from '@angular/material/select';
 import { RouterModule } from '@angular/router';
 
-import { CommonServiceStubModule } from '../../testing/common-service-stub/common-service-stub.module'
-
-import { AddDaysComponent } from './add-days/add-days.component';
 import { AppComponent } from './app.component';
-import { CountDaysComponent } from './count-days/count-days.component';
 import { TopBarComponent } from './top-bar/top-bar.component';
-import { FeedbackComponent } from './feedback/feedback.component';
 
 describe('AppComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [
-        AddDaysComponent, AppComponent, CountDaysComponent, TopBarComponent, FeedbackComponent
+        AppComponent, TopBarComponent
       ],
       imports: [
-        BrowserAnimationsModule,
-        CommonServiceStubModule,
-        FormsModule,
-        MatDatepickerModule,
-        MatFormFieldModule,
-        MatCheckboxModule,
-        MatRadioModule,
-        MatSelectModule,
-        MatInputModule,
-        MatTabsModule,
-        MatNativeDateModule,
-        ReactiveFormsModule,
         RouterModule.forRoot([
           { path: '', component: AppComponent },
         ])
       ],
-      providers: [
-        {provide: MAT_DATE_LOCALE, useValue: 'en-au'},
-        {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
-        {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS}
-      ]
     }).compileComponents();
   }));
 
--- src/app/home/home.component.spec.ts
+++ src/app/home/home.component.spec.ts
@@ -1,5 +1,21 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { DateAdapter, MAT_DATE_FORMATS, MAT_DATE_LOCALE, MatNativeDateModule } from '@angular/material/core';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { MatCheckboxModule, MatInputModule, MatTabsModule } from '@angular/material';
+import { MatDatepickerModule } from '@angular/material/datepicker';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MAT_MOMENT_DATE_FORMATS, MomentDateAdapter } from '@angular/material-moment-adapter';
+import { MatRadioModule } from '@angular/material/radio';
+import { MatSelectModule } from '@angular/material/select';
+import { RouterModule } from '@angular/router';
+
+import { CommonServiceStubModule } from '../../../testing/common-service-stub/common-service-stub.module'
+
+import { AddDaysComponent } from '../add-days/add-days.component';
+import { CountDaysComponent } from '../count-days/count-days.component';
+import { FeedbackComponent } from '../feedback/feedback.component';
 import { HomeComponent } from './home.component';
 
 describe('HomeComponent', () => {
@@ -8,7 +24,31 @@ describe('HomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HomeComponent ]
+      declarations: [
+        AddDaysComponent, CountDaysComponent, FeedbackComponent, HomeComponent
+      ],
+      imports: [
+        BrowserAnimationsModule,
+        CommonServiceStubModule,
+        FormsModule,
+        MatDatepickerModule,
+        MatFormFieldModule,
+        MatCheckboxModule,
+        MatRadioModule,
+        MatSelectModule,
+        MatInputModule,
+        MatTabsModule,
+        MatNativeDateModule,
+        ReactiveFormsModule,
+        RouterModule.forRoot([
+          { path: '', component: HomeComponent },
+        ])
+      ],
+      providers: [
+        {provide: MAT_DATE_LOCALE, useValue: 'en-au'},
+        {provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE]},
+        {provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS}
+      ]
     })
     .compileComponents();
   }));
